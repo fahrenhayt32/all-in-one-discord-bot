561aed23ef048342383ff7f24ce0913cc9bd0d36
Fahrenhayt <84893283+fahrenhayt32@users.noreply.github.com>
2021-05-28 13:49:46 +0300

    Update wl-ver.js

--- fahrenheit-all-in-one/komutlar/moderasyon/wl-ver.js
+++ fahrenheit-all-in-one/komutlar/moderasyon/wl-ver.js
@@ -3,12 +3,19 @@ module.exports = {
     code: `
     $if[$mentioned[1]!=]
     $title[! Bir üyeye WL verildi | $getVar[sunucuismi]]
-    
-    $description[
-    $timezone[Europe/Istanbul]<@$authorID> adlı yetkili <@$mentioned[1]> isimli üyeye $sum[$hour;3].$minute saatinde wl verdi!!]
+
+    $timezone[Europe/Istanbul]
+    $addField[WL Verilme Saati:; \`\`\`$sum[$hour;3].$minute \`\`\`;yes]
+    $addField[İşlemi Gerçekleştiren:;\`\`\`$username[$authorID]\`\`\`;yes]
+    $addField[Kayıt Edilen Üye:;\`\`\`$username[$mentioned[1]]\`\`\`;yes]
+    $addField[Yetkilinin Yaptığı Toplam Kayıt:;\`\`\`$sum[$getUserVar[wl_kayit];1]\`\`\`;yes]
+    $addField[Verilen rol;<@&$getVar[whitelistrol]>;yes]
+    $addField[Alınan rol;<@&$getVar[kayıtsızrol]>;yes]
+    $setUserVar[wl_kayit;$sum[$getUserVar[wl_kayit];1]]
     $useChannel[$getVar[wlrollog]]
     $onlyPerms[managemessages;bu komutu kullanman için "mesajları yönet" yetki iznin olmalı]
     $giveRole[$mentioned[1];$getVar[whitelistrol]]
+    $takeRole[$mentioned[1];$getVar[kayıtsızrol]]
     $thumbnail[$serverIcon]
     $color[f3892]
     $suppressErrors[bir hata meydana geldi ! ya rol bulunamadı yada bu rolü vermek için gerekli yetkiye sahip değilim]
